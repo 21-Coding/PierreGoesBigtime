180eaf9c9795410f4e534c2d050f3513802f87ca
Dom <Dom@Tiffanys-MacBook-Pro-2.local>
2020-03-13 11:38:54 -0700

    add Vendor class functionality.

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -3,5 +3,39 @@ using Systems.Collections.Generic;
 
 namespace VendorTracker.Models 
 {
+    public class Vendor
+    {
+        private static List<Vendor> _instances = new List<Vendor> {};
+        public string Representative { get; set; }
+        public int Id { get; }
+        public List<Order> Orders { get; set; }
 
+        public Vendor(string Name)
+        {
+            Name = name;
+            _instances.Add(this);
+            Id = _instances.Count;
+            Orders = new List<Order> { };
+        }
+
+        public static void ClearAll()
+        {
+            _instances.ClearAll();
+        }
+
+        public static List<Vendor> GetAll()
+        {
+            return _instances; 
+        }
+
+        public static Vendor Find(int searchId)
+        {
+            return _instances[searchId -1];
+        }
+
+        public void Add(Order order)
+        {
+            Orders.Add(order);
+        }
+    }
 }
