ca1284b205142557cab238558f64cf42e44a29a3
Dom <Dom@Tiffanys-MacBook-Pro-2.local>
2020-03-13 11:23:25 -0700

    add functionality to Order class by adding constructors, an order list, and some crud.

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -3,5 +3,38 @@ using Systems.Collections.Generic;
 
 namespace VendorTracker.Models 
 {
+    public class Order
+    {
+        public string Info { get; set; }
+        public int Id { get; set; }
+        private static List<Order> _instances = new List<Order> { };
+
+
+        public Order(string info)
+        {
+            Info = info;
+            _instances.Add(this);
+            Id = _instances.Count;
+        }
+
+        public static List<Order> GetAll()
+        {
+            return _instances; 
+        }
+
+        public static void ClearAll()
+        {
+            return _instances.Clear();
+        }
+
+        public static Order Find(int searchId)
+        {
+            return _instances[searchId - 1];
+            
+        }
+    }
+
+
+
 
 }
